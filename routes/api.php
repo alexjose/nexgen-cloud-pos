69e8beffeab3fc4c285265a343ea90745824eb33
Alex Jose <alexjose4u@gmail.com>
2019-03-29 18:57:38 +0530

    Passport configured

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function (Request $request){
+    return ['message' => 'NexGen Cloud POS Billing Solution API v1'];
+});
